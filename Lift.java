5a0898c84cc0635e0364840f3496dee595e558d5
Pranit5895 <guptapranit9@gmail.com>
2020-05-05 20:17:34 -0700

    main functiona completed

--- Lift.java
+++ Lift.java
@@ -20,3 +20,20 @@
 //3rd floor calls, Lift A(0) goes there.
 
 //5th  floor calls, Lift A(3) and Lift B(7) are equidistant from 5, so Lift from l//owest floor goes there i.e. A.
+
+import java.util.Scanner
+
+public class lift{
+	public static void main(String args[])
+	{
+		Scanner in= new Scanner(System.in);
+		System.out.println("Etner the max no of floors:- ");
+		int tf=in.nextInt();
+		int A=0,B=tf;
+		System.out.println("Enter the number of test cases that you want to run");
+		int test = in.nextInt();
+
+		for (int i = 0; i < test; i++) {
+			lift_function(lift_A, lift_B, top_floor);
+		}
+	}
