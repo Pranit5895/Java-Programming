0533b1d23948c3d6a6ecdab2dce299a044e7a46e
Pranit5895 <guptapranit9@gmail.com>
2020-05-05 20:18:24 -0700

    lift_function() completed hence solution done

--- Lift.java
+++ Lift.java
@@ -36,4 +36,41 @@ public class lift{
 		for (int i = 0; i < test; i++) {
 			lift_function(lift_A, lift_B, top_floor);
 		}
+	
 	}
+	public static void lift_function(int A, int B, int tf)
+{
+	Scanner inp=new Scanner(System.in);
+	System.out.println("Which floor :- ");
+	int call=inp.nextInt();
+	if(call<=tf) {
+		if(call>A && call<B) {
+			if(call-A < B-call) {
+				System.out.println("Lift");
+				A=call;
+			}
+			else if(call-A > b-call) {
+				system.out.ptintln("Lift");
+				A=call;
+			}
+		}
+		else if(call>A && call>B) {
+			if(a-call< call-b){
+				system.out.println("Lift");
+				A=call;
+			}
+			else if (A-call > call-B) {
+				System.out.println("Lift");
+				B=call;
+			}
+			else {
+				System.out.println("Lift");
+				A=call;
+			}
+		}
+	}
+	else {
+		System.out.println("Invalid floor");
+	}
+}
+}
