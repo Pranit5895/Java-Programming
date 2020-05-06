e66e4f8dc64895cbb53718b09e01d1ccd9b654c6
Pranit5895 <guptapranit9@gmail.com>
2020-05-05 20:10:18 -0700

    Class declared and input done

---
+++ Arithmetic.java
@@ -0,0 +1,8 @@
+public class Arithmetic {
+ 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
