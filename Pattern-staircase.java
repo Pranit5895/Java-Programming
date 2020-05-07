9d7e53914b40176bf732d5b1b48571eb5bc7d2b1
Pranit5895 <guptapranit9@gmail.com>
2020-05-06 23:02:40 -0700

    Question added with sample output

---
+++ Pattern-staircase.java
@@ -0,0 +1,8 @@
+//Q2) Task to will be that you have to genrate a staircase patter by taking (n) as input and where n=lenght of the stair case 
+//for EX:- n=6 so   # 
+//                 ## 
+//                ### 
+//               #### 
+//              ##### 
+//             ######
+
