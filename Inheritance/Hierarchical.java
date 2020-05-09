bb6fc0d5dbdca74e56f5a39e5987331065116017
Pranit5895 <guptapranit9@gmail.com>
2020-05-09 04:28:07 -0700

    Class two and Class three inherited from base class

--- Inheritance/Hierarchical.java
+++ Inheritance/Hierarchical.java
@@ -10,4 +10,16 @@ class one
     { 
         System.out.println("Geeks"); 
     } 
-} 
+}
+class two extends one
+{
+    public void print_for()
+    {
+        System.out.println("for");
+    }
+}
+
+class three extends one
+{
+    /*............*/
+}
