a6212eb640ed3e70b1ec2e3547b87ada43ea3dcb
Pranit5895 <guptapranit9@gmail.com>
2020-05-09 04:18:19 -0700

    base class declared

---
+++ Inheritance/Single.java
@@ -0,0 +1,13 @@
+//Java program to illustrate the  
+// concept of single inheritance 
+import java.util.*; 
+import java.lang.*; 
+import java.io.*; 
+  
+class one 
+{ 
+    public void print_geek() 
+    { 
+        System.out.println("Geeks"); 
+    } 
+} 
