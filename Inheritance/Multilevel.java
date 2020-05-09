49aa2e644190d12f825e44dd5027c5cf0f2a940d
Pranit5895 <guptapranit9@gmail.com>
2020-05-09 04:24:48 -0700

    Multi level inheritance code completed

--- Inheritance/Multilevel.java
+++ Inheritance/Multilevel.java
@@ -25,3 +25,14 @@ class three extends two
         System.out.println("Geeks");
     }
 }
+// Drived class
+public class Main
+{
+    public static void main(String[] args)
+    {
+        three g = new three();
+        g.print_geek();
+        g.print_for();
+        g.print_geek();
+    }
+}
