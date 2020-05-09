7be51427a6058ac8d8c840783c8bddae903bb92d
Pranit5895 <guptapranit9@gmail.com>
2020-05-09 04:27:12 -0700

    Base class declared

---
+++ Inheritance/Hierarchical.java
@@ -0,0 +1,13 @@
+// Java program to illustrate the  
+// concept of Hierarchical inheritance 
+import java.util.*; 
+import java.lang.*; 
+import java.io.*; 
+  
+class one 
+{ 
+    public void print_geek() 
+    { 
+        System.out.println("Geeks"); 
+    } 
+} 
