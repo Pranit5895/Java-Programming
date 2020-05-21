a08bf43c7da524ed15ff12a08b0debb6c31e2a18
Pranit5895 <guptapranit9@gmail.com>
2020-05-21 05:46:09 -0700

    Input from user

---
+++ Product.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        /* This reads the input provided by user
+         * using keyboard
+         */
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+
+        // This method reads the number provided using keyboard
+        int num1 = scan.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = scan.nextInt();
