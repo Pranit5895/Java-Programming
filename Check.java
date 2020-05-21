cd284da7fff28efe76281a24bd1ea7c84ba41ba6
Pranit5895 <guptapranit9@gmail.com>
2020-05-21 05:58:25 -0700

    Conditional checking started

--- Check.java
+++ Check.java
@@ -8,3 +8,10 @@ class CheckEvenOdd
     System.out.println("Enter an Integer number:");
 
     //The input provided by user is stored in num
+    Scanner input = new Scanner(System.in);
+    num = input.nextInt();
+
+    /* If number is divisible by 2 then it's an even number
+     * else odd number*/
+    if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
