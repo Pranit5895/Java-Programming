1d557e2e4c314c1a43bb62358d914591997a38df
Pranit5895 <guptapranit9@gmail.com>
2020-05-21 05:57:17 -0700

    Input from user taken

---
+++ Check.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class CheckEvenOdd
+{
+  public static void main(String args[])
+  {
+    int num;
+    System.out.println("Enter an Integer number:");
+
+    //The input provided by user is stored in num
