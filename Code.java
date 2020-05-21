58a5e09acc52c725807cfc3b33f2745be9afe79b
Pranit5895 <guptapranit9@gmail.com>
2020-05-21 05:52:04 -0700

    Input from user for switch case checking

---
+++ Code.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class JavaExample
+{
+   public static void main(String[ ] arg)
+   {
+	boolean isVowel=false;;
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("Enter a character : ");
+	char ch=scanner.next().charAt(0); 
+	scanner.close();
