23d25cc2b326e443e709bbd4a55936486f5df924
Pranit5895 <guptapranit9@gmail.com>
2020-05-21 05:48:01 -0700

    Input year from user

---
+++ Leap-year.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Demo {
+
+    public static void main(String[] args) {
+
+    	int year;
+    	Scanner scan = new Scanner(System.in);
+    	System.out.println("Enter any Year:");
+    	year = scan.nextInt();
+    	scan.close();
