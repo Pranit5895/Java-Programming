db2713b65d7ab1006e7979507bee2b0b76ac5346
Pranit5895 <guptapranit9@gmail.com>
2020-05-23 07:54:47 -0700

    Input text from user done

--- File.java
+++ File.java
@@ -5,4 +5,10 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.util.Scanner;
+public class Files {
 
+    public static void main(String args[]){
+
+        System.out.print("Enter Text: ");
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
