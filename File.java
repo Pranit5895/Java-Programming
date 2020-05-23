df32b0da65fefb3db493200f87d4648aa67f9845
Pranit5895 <guptapranit9@gmail.com>
2020-05-23 07:55:34 -0700

    Confirmation of file being exist

--- File.java
+++ File.java
@@ -12,3 +12,18 @@ public class Files {
         System.out.print("Enter Text: ");
         Scanner scan = new Scanner(System.in);
         String text = scan.nextLine();
+	FileWriter fWriter = null;
+        BufferedWriter writer = null;
+        try {
+          fWriter = new FileWriter("newfile.txt");
+          writer = new BufferedWriter(fWriter);
+          writer.write(text);
+          writer.newLine();
+          writer.close();
+          System.err.println("Your input of " + text.length() + " characters was saved.");
+        } catch (Exception e) {
+            System.out.println("Error!");
+        }
+
+	Scanner in= new Scanner(System.in);
+	str pass= in.nextInt(); //input password
