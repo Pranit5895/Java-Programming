904aafc45d9a8bbe735396b3cf971511f510c296
Pranit5895 <guptapranit9@gmail.com>
2020-05-23 07:48:33 -0700

    Input from user as array

---
+++ Sorting-algos.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Sorting{
+	public static void main(String[] args)
+	{
+		Scanner scan= new Scanner(System.in);
+		int size = scan.nextInt();
+		int array[] = new int[size];
+		for(inti=0; i<array.length; i++)
+		{
+			array[i]=scan.nextInt();
+		}
