4240122e42beba570484ac801d0f6604df5302c4
Pranit5895 <guptapranit9@gmail.com>
2020-05-23 07:56:18 -0700

    Try block for checking file path

--- File.java
+++ File.java
@@ -27,3 +27,26 @@ public class Files {
 
 	Scanner in= new Scanner(System.in);
 	str pass= in.nextInt(); //input password
+	// Specify the path of the file here
+		File file = new File("D:\\JAVA\\newfile.txt");
+		BufferedInputStream bis = null;
+		FileInputStream fis = null;
+
+		try {
+			// FileInputStream to read the file
+			fis = new FileInputStream(file);
+
+			/*
+			 * Passed the FileInputStream to BufferedInputStream For Fast read using the
+			 * buffer array.
+			 */
+			bis = new BufferedInputStream(fis);
+
+			/*
+			 * available() method of BufferedInputStream returns 0 when there are no more
+			 * bytes present in the file to be read
+			 */
+			while (bis.available() > 0) {
+				System.out.print((char) bis.read());
+			}
+
