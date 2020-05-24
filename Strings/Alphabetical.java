c7193240e22947f7e964d4467f79c5854c731822
Pranit5895 <guptapranit9@gmail.com>
2020-05-24 07:44:30 -0700

    User enterting string and storing in an array

--- Strings/Alphabetical.java
+++ Strings/Alphabetical.java
@@ -13,3 +13,11 @@ public class JavaExample
 
         String str[] = new String[count];
         Scanner scan2 = new Scanner(System.in);
+	 //User is entering the strings and they are stored in an array
+        System.out.println("Enter the Strings one by one:");
+        for(int i = 0; i < count; i++)
+        {
+            str[i] = scan2.nextLine();
+        }
+        scan.close();
+        scan2.close();
