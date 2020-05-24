6bdc2cd0c0b7b99e95900ff1752b1bd20a300911
Pranit5895 <guptapranit9@gmail.com>
2020-05-24 07:58:29 -0700

    Loop for calculating average of numbers in an array

--- Average.java
+++ Average.java
@@ -3,3 +3,12 @@ public class JavaExample {
     public static void main(String[] args) {
         double[] arr = {19, 12.89, 16.5, 200, 13.7};
         double total = 0;
+	 for(int i=0; i<arr.length; i++){
+        	total = total + arr[i];
+        }
+
+
+        /* arr.length returns the number of elements
+         * present in the array
+         */
+        double average = total / arr.length;
