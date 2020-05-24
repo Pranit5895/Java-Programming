a685d7a5f36dee056fb590fc7d238fa017465144
Pranit5895 <guptapranit9@gmail.com>
2020-05-24 07:50:54 -0700

    Input from user taken

--- Strings/Palindrome-using-queue.java
+++ Strings/Palindrome-using-queue.java
@@ -6,3 +6,7 @@ class PalindromeTest {
     public static void main(String[] args) {
 
     	System.out.print("Enter any string:");
+	Scanner in=new Scanner(System.in);
+        String inputString = in.nextLine();
+        Queue queue = new LinkedList();
+
