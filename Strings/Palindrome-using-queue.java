8764332a44a119f22306c32b725a684c4d30f1f4
Pranit5895 <guptapranit9@gmail.com>
2020-05-24 07:50:21 -0700

    Code started

---
+++ Strings/Palindrome-using-queue.java
@@ -0,0 +1,8 @@
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.LinkedList;
+class PalindromeTest {
+
+    public static void main(String[] args) {
+
+    	System.out.print("Enter any string:");
