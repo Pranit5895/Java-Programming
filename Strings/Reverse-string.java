65baa4795a6a07f16fc3d73b59b70e33f4959dda
Pranit5895 <guptapranit9@gmail.com>
2020-05-24 07:54:11 -0700

    Code started with class definition

---
+++ Strings/Reverse-string.java
@@ -0,0 +1,10 @@
+public class Example
+{
+   public void reverseWordInMyString(String str)
+   {
+	/* The split() method of String class splits
+	 * a string in several strings based on the
+	 * delimiter passed as an argument to it
+	 */
+	String[] words = str.split(" ");
+	String reversedString = "";
