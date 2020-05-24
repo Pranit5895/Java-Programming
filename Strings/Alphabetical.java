fabfdda4a7f054001f90e6ab4ded7ceca3f04c46
Pranit5895 <guptapranit9@gmail.com>
2020-05-24 07:42:57 -0700

    Input from user as string

---
+++ Strings/Alphabetical.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class JavaExample
+{
+    public static void main(String[] args) 
+    {
+        int count;
+        String temp;
+        Scanner scan = new Scanner(System.in);
