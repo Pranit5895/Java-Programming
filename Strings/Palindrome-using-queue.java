a1d6b8b6651a6cbe4b5c5199de598cc637d85abf
Pranit5895 <guptapranit9@gmail.com>
2020-05-24 07:51:52 -0700

    Output

--- Strings/Palindrome-using-queue.java
+++ Strings/Palindrome-using-queue.java
@@ -18,3 +18,10 @@ for (int i = inputString.length()-1; i >=0; i--) {
         while (!queue.isEmpty()) {
             reverseString = reverseString+queue.remove();
         }
+	if (inputString.equals(reverseString))
+            System.out.println("The input String is a palindrome.");
+        else
+            System.out.println("The input String is not a palindrome.");
+
+    }
+}
