4076209133e8b3e3bb84fc630c6e44233e9cb5a3
Pranit5895 <guptapranit9@gmail.com>
2020-05-24 07:43:42 -0700

    user will be asked to enter the count of strings

--- Strings/Alphabetical.java
+++ Strings/Alphabetical.java
@@ -6,3 +6,10 @@ public class JavaExample
         int count;
         String temp;
         Scanner scan = new Scanner(System.in);
+	//User will be asked to enter the count of strings
+        System.out.print("Enter number of strings you would like to enter:");
+        count = scan.nextInt();
+
+
+        String str[] = new String[count];
+        Scanner scan2 = new Scanner(System.in);
