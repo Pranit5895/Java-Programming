b48975b9a46b2ed613e76da74b1a23e171322db2
Pranit5895 <guptapranit9@gmail.com>
2020-05-24 07:48:00 -0700

    Input from user

---
+++ Strings/Palindrome-using-stack.java
@@ -0,0 +1,10 @@
+import java.util.Stack;
+import java.util.Scanner;
+class PalindromeTest {
+
+    public static void main(String[] args) {
+
+    	System.out.print("Enter any string:");
+        Scanner in=new Scanner(System.in);
+        String inputString = in.nextLine();
+        Stack stack = new Stack();
