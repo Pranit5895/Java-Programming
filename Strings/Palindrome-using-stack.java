f6b53c1464b96bf3a5d1359ba94e567a0eceecc1
Pranit5895 <guptapranit9@gmail.com>
2020-05-24 07:48:36 -0700

    Conditional checking done

--- Strings/Palindrome-using-stack.java
+++ Strings/Palindrome-using-stack.java
@@ -8,3 +8,12 @@ class PalindromeTest {
         Scanner in=new Scanner(System.in);
         String inputString = in.nextLine();
         Stack stack = new Stack();
+	for (int i = 0; i < inputString.length(); i++) {
+            stack.push(inputString.charAt(i));
+        }
+
+        String reverseString = "";
+
+        while (!stack.isEmpty()) {
+            reverseString = reverseString+stack.pop();
+        }
