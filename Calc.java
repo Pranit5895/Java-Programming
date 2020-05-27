8c25250d5b45f157925cff683ee910a966664174
Pranit5895 <guptapranit9@gmail.com>
2020-05-26 20:51:10 -0700

    Input from user

--- Calc.java
+++ Calc.java
@@ -2,3 +2,7 @@ package exp2;
 
 public class calc {
 	public static void main(String[] args) 
+		int num1 = int.parseInt(args[0]);
+		String operator = args[1];
+		int num2 = int.parseInt(args[2]);
+		int operation = 0;
