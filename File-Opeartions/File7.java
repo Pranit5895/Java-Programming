c58cb56251c0c2e7a974ce057ffc8f4bfde2ee82
Pranit5895 <guptapranit9@gmail.com>
2020-05-27 01:47:56 -0700

    Convert the file into wrtie only mode again

---
+++ File-Opeartions/File7.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+
+public class File9 {
+	public static void main(String[] args) {
+
+		File myfile = new File("G:\\HACKERRANK\\JAVAFORDUMMIES\\src\\DAY5\\newfile.txt");
+		// changing the file mode to writable
+		myfile.setWritable(true);
+		if (myfile.canWrite()) {
+			System.out.println("File is again writable.");
+		} else {
+			System.out.println("File is still read only.");
+		}
+	}
+}
