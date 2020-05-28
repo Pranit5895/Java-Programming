319454613c98a8c0fe5105a59d0d46524e6b3aff
Pranit5895 <guptapranit9@gmail.com>
2020-05-27 21:59:20 -0700

    How to read a file method one

--- File-Opeartions/File8.java
+++ File-Opeartions/File8.java
@@ -17,4 +17,28 @@ public class File5 {
 				System.out.println(contentLine);
 				contentLine = br.readLine();
 			}
+br2 = new BufferedReader(new FileReader("G:\\HACKERRANK\\JAVAFORDUMMIES\\src\\DAY6\\newfile.txt"));
 
+			// Second way of reading the file
+			System.out.println("Reading the file using read() method(2):");
+			int num = 0;
+			char ch;
+			while ((num = br2.read()) != -1) {
+				ch = (char) num;
+				System.out.print(ch);
+			}
+
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+				if (br2 != null)
+					br2.close();
+			} catch (IOException ioe) {
+				System.out.println("Error in closing the BufferedReader");
+			}
+		}
+	}
+}
