54713107a5c9431aa0bd593cef5f43698716dbc6
Pranit5895 <guptapranit9@gmail.com>
2020-05-29 03:03:36 -0700

    Example 2

--- RegEx/RegEx2.java
+++ RegEx/RegEx2.java
@@ -13,3 +13,9 @@ public class Regex2 {
 		System.out.println(Pattern.matches("[789][0-9]{9}", "99530389490"));// false (11 characters)
 		System.out.println(Pattern.matches("[789][0-9]{9}", "6953038949"));// false (starts from 6)
 		System.out.println(Pattern.matches("[789][0-9]{9}", "8853038949"));// true
+		System.out.println("by metacharacters ...");
+		System.out.println(Pattern.matches("[789]{1}\\d{9}", "8853038949"));// true
+		System.out.println(Pattern.matches("[789]{1}\\d{9}", "3853038949"));// false (starts from 3)
+
+	}
+}
