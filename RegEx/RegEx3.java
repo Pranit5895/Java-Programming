c20cd47d2581e48ba0c315eda865e2c69cf85929
Pranit5895 <guptapranit9@gmail.com>
2020-05-29 03:04:53 -0700

    Example 3

--- RegEx/RegEx3.java
+++ RegEx/RegEx3.java
@@ -9,3 +9,7 @@ public class Regex3 {
 	public static void main(String args[]) {
 		System.out.println(Pattern.matches("[a-zA-Z0-9]{6}", "arun32"));// true
 		System.out.println(Pattern.matches("[a-zA-Z0-9]{6}", "kkvarun32"));// false (more than 6 char)
+		System.out.println(Pattern.matches("[a-zA-Z0-9]{6}", "JA2Uk2"));// true
+		System.out.println(Pattern.matches("[a-zA-Z0-9]{6}", "arun$2"));// false ($ is not matched)
+	}
+}
