a4bd13a5062ea5f3e3f6378effb3db1c59c587b5
Pranit5895 <guptapranit9@gmail.com>
2020-06-05 01:46:53 -0700

    Hackerrank solution for Java

---
+++ Hackerrank/Java-Stdin-and-Stdout-1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      int c=sc.nextInt();
+      //Complete this line
+      //Complete this line
+        
+      System.out.println(a);
+       System.out.println(b);
+        System.out.println(c);
+      //Complete this line
+      //Complete this line
+    }
+}
