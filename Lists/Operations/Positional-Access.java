9c6fe72b8f1e19f28a83901d6cf7bb99274c6e03
Pranit5895 <guptapranit9@gmail.com>
2020-06-20 01:07:37 -0700

    Creating naother list

--- Lists/Operations/Positional-Access.java
+++ Lists/Operations/Positional-Access.java
@@ -8,3 +8,9 @@ public class ListDemo {
         l1.add(0, 1); // adds 1 at 0 index 
         l1.add(1, 2); // adds 2 at 1 index 
         System.out.println(l1); // [1, 2] 
+
+	// Creating another list
+        List<Integer> l2 = new ArrayList<Integer>();
+        l2.add(1);
+        l2.add(2);
+        l2.add(3);
