b46f6bad4e1d67c8bcfca68c1f8c3d8a43065a12
Pranit5895 <guptapranit9@gmail.com>
2020-06-20 01:14:09 -0700

    Type safe array list, stores only strings

--- Lists/Operations/Search.java
+++ Lists/Operations/Search.java
@@ -1,3 +1,11 @@
 import java.util.*; 
   
 public class ListDemo { 
+	public static void main(String[] args)
+    {
+        // Type safe array list, stores only string
+        List<String> l = new ArrayList<String>(5);
+        l.add("Geeks");
+        l.add("for");
+        l.add("Geeks");
+
