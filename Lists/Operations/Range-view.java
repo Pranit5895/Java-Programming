3bb4d16ecad507e2aae10d5090534c9c59fa2f1d
Pranit5895 <guptapranit9@gmail.com>
2020-06-21 00:24:22 -0700

    Type safe array list, stores only string

--- Lists/Operations/Range-view.java
+++ Lists/Operations/Range-view.java
@@ -1,2 +1,14 @@
 import java.util.*; 
-public class ListDemo { 
+public class ListDemo {
+	public static void main(String[] args)
+    {
+        // Type safe array list, stores only string
+        List<String> l = new ArrayList<String>(5);
+
+        l.add("GeeksforGeeks");
+        l.add("Practice");
+        l.add("GeeksQuiz");
+        l.add("IDE");
+        l.add("Courses");
+
+        List<String> range = new ArrayList<String>();
