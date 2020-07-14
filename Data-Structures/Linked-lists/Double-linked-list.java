59d5fe9c9a8b5e787236100b91ea872984621b1f
Pranit5895 <guptapranit9@gmail.com>
2020-07-14 08:15:41 -0700

    Constructor to create a new node next and prev is by default initialized as null

--- Data-Structures/Linked-lists/Double-linked-list.java
+++ Data-Structures/Linked-lists/Double-linked-list.java
@@ -6,3 +6,7 @@ public class DLL {
         int data;
         Node prev;
         Node next;
+	 // Constructor to create a new node
+        // next and prev is by default initialized as null
+        Node(int d) { data = d; }
+    }
