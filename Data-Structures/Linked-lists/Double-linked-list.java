721e0816d5f4c50c017856f39865afabc3c8e641
Pranit5895 <guptapranit9@gmail.com>
2020-07-14 08:19:08 -0700

    Add a node at the end of the list

--- Data-Structures/Linked-lists/Double-linked-list.java
+++ Data-Structures/Linked-lists/Double-linked-list.java
@@ -55,3 +55,34 @@ public class DLL {
         if (new_node.next != null)
             new_node.next.prev = new_node;
     }
+     // Add a node at the end of the list
+    void append(int new_data)
+    {
+        /* 1. allocate node
+        * 2. put in the data */
+        Node new_node = new Node(new_data);
+
+        Node last = head; /* used in step 5*/
+
+        /* 3. This new node is going to be the last node, so
+        * make next of it as NULL*/
+        new_node.next = null;
+
+        /* 4. If the Linked List is empty, then make the new
+        * node as head */
+        if (head == null) {
+            new_node.prev = null;
+            head = new_node;
+            return;
+        }
+
+        /* 5. Else traverse till the last node */
+        while (last.next != null)
+            last = last.next;
+
+        /* 6. Change the next of last node */
+        last.next = new_node;
+
+        /* 7. Make last node as previous of new node */
+        new_node.prev = last;
+    }
