d2f077fabb2a23ef60671b9c0858c02bc2407d2f
Pranit5895 <guptapranit9@gmail.com>
2020-07-14 08:16:35 -0700

    Adding a node at the front of the list

--- Data-Structures/Linked-lists/Double-linked-list.java
+++ Data-Structures/Linked-lists/Double-linked-list.java
@@ -10,3 +10,21 @@ public class DLL {
         // next and prev is by default initialized as null
         Node(int d) { data = d; }
     }
+    // Adding a node at the front of the list
+    public void push(int new_data)
+    {
+        /* 1. allocate node
+        * 2. put in the data */
+        Node new_Node = new Node(new_data);
+
+        /* 3. Make next of new node as head and previous as NULL */
+        new_Node.next = head;
+        new_Node.prev = null;
+
+        /* 4. change prev of head node to new node */
+        if (head != null)
+            head.prev = new_Node;
+
+        /* 5. move the head to point to the new node */
+        head = new_Node;
+    }
