0273b63a0007e50409c07ac2676b24f7e85a5d2f
Pranit5895 <guptapranit9@gmail.com>
2020-07-14 08:20:01 -0700

    This function prints contents of linked list starting from the given node

--- Data-Structures/Linked-lists/Double-linked-list.java
+++ Data-Structures/Linked-lists/Double-linked-list.java
@@ -86,3 +86,20 @@ public class DLL {
         /* 7. Make last node as previous of new node */
         new_node.prev = last;
     }
+     // This function prints contents of linked list starting from the given node
+    public void printlist(Node node)
+    {
+        Node last = null;
+        System.out.println("Traversal in forward Direction");
+        while (node != null) {
+            System.out.print(node.data + " ");
+            last = node;
+            node = node.next;
+        }
+        System.out.println();
+        System.out.println("Traversal in reverse direction");
+        while (last != null) {
+            System.out.print(last.data + " ");
+            last = last.prev;
+        }
+    }
