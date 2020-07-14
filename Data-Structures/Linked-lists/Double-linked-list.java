c64fff98eb22d88d1e417d95b563c7b85b1c7965
Pranit5895 <guptapranit9@gmail.com>
2020-07-14 08:20:44 -0700

    Driver program to test above functions

--- Data-Structures/Linked-lists/Double-linked-list.java
+++ Data-Structures/Linked-lists/Double-linked-list.java
@@ -103,3 +103,28 @@ public class DLL {
             last = last.prev;
         }
     }
+    /* Driver program to test above functions*/
+    public static void main(String[] args)
+    {
+        /* Start with the empty list */
+        DLL dll = new DLL();
+
+        // Insert 6. So linked list becomes 6->NULL
+        dll.append(6);
+
+        // Insert 7 at the beginning. So linked list becomes 7->6->NULL
+        dll.push(7);
+
+        // Insert 1 at the beginning. So linked list becomes 1->7->6->NULL
+        dll.push(1);
+
+        // Insert 4 at the end. So linked list becomes 1->7->6->4->NULL
+        dll.append(4);
+
+        // Insert 8, after 7. So linked list becomes 1->7->8->6->4->NULL
+        dll.InsertAfter(dll.head.next, 8);
+
+        System.out.println("Created DLL is: ");
+        dll.printlist(dll.head);
+    }
+}
