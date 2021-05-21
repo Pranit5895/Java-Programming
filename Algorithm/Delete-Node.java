0ce6271cac41d4bb35aa0a992296941fe5ce33f5
Pranit5895 <guptapranit9@gmail.com>
2021-05-21 13:41:34 +0530

    Delete Node Code

---
+++ Algorithm/Delete-Node.java
@@ -0,0 +1,10 @@
+class Solution {
+    public ListNode deleteNode(ListNode head, int val) {
+        if (head == null)
+            return head;
+        if (head.val == val)
+            return head.next;
+        head.next = deleteNode(head.next, val);
+        return head;
+    }
+}
