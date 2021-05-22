be3720915b829355e874b5f5880c4678e81e2477
Pranit5895 <guptapranit9@gmail.com>
2021-05-22 17:04:17 +0530

    Outside Package by sub class only  Access Specifier

---
+++ OOPS/Access-Specifiers/OutsidePackageBySubClassOnly.java
@@ -0,0 +1,9 @@
+import AccessSpecifiers.WithinClass;
+import DataMembers;
+
+public class OutsidePackageBySubClassOnly {
+	public static void main(String[] args) {
+		DataMembers dm1 = new DataMembers();
+		dm1.printmembers();
+	}
+}
