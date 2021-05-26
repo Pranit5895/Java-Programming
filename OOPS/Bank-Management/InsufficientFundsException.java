a62382e1eb228202442fdaeee7715b8feffd72c8
Pranit5895 <guptapranit9@gmail.com>
2021-05-26 19:59:59 +0530

    Bank Management Code

---
+++ OOPS/Bank-Management/InsufficientFundsException.java
@@ -0,0 +1,15 @@
+package BankManagement;
+
+import java.io.*;
+
+public class InsufficientFundsException extends Exception {
+   private double amount;
+   
+   public InsufficientFundsException(double amount) {
+      this.amount = amount;
+   }
+   
+   public double getAmount() {
+      return amount;
+   }
+}
