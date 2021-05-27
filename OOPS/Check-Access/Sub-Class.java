7204ce0c79702292f1c48e77143e7c42e8dc9ebf
Pranit5895 <guptapranit9@gmail.com>
2021-05-27 19:32:39 +0530

    Sub Class Code

---
+++ OOPS/Check-Access/Sub-Class.java
@@ -0,0 +1,11 @@
+package SubPackage;
+import CheckAccess.Access_Specifiers;
+public class SubClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
