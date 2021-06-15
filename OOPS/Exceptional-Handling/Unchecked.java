4bf9f01c52f0c3d7aded7e069c4ddedfa04a351e
Pranit5895 <guptapranit9@gmail.com>
2021-06-15 19:56:09 +0530

    Unchecked Code

---
+++ OOPS/Exceptional-Handling/Unchecked.java
@@ -0,0 +1,11 @@
+package ExceptionHandling;
+
+public class Unchecked {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=10;
+		int num2=0;
+		int res=num1/num2;
+		System.out.println(res);
+	}
+}
